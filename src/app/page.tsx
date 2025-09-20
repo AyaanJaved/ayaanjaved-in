0b7f012c4fbb0cbbc53e144abde96d7dbaddb171
AyaanJaved <mail.ayaanjaved@gmail.com>
2025-09-20 14:52:17 +0530

    refactor: remove placeholder content from Home component to streamline layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,6 @@ export default function Home() {
   return (
     <main>
       <HeroContainer />
-      
-      <div className="p-20 text-center">
-        <h2 className="text-2xl">The page content starts here...</h2>
-      </div>
     </main>
   );
 }
